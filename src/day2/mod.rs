35a1314ef5b69121ccd0e1e107b98d9738e7204a
Ducksan Cho <ducktyper@gmail.com>
2022-12-14 22:23:16 +1300

    day2 - refactor

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -1,14 +1,69 @@
 #[allow(dead_code)]
-pub fn total_score(input: &str) -> i32 {
-    games(input).iter().map(|game| game.score()).sum()
+pub fn total_score(input: &str, strategy: StrategyType) -> i32 {
+    games(input, strategy).iter().map(|game| game.score()).sum()
 }
 
-fn games(input: &str) -> Vec<Game> {
-    Reader { input: input }.games()
+pub enum StrategyType {
+    MapToPlay,
+    MapToOutcome,
+}
+
+fn games(input: &str, strategy: StrategyType) -> Vec<Game> {
+    Reader {
+        input: input,
+        strategy_type: strategy,
+    }
+    .games()
+}
+
+trait Strategy {
+    fn you(&self, opponent: &Play, value: &str) -> Play;
+}
+
+struct MapToPlay {}
+
+impl Strategy for MapToPlay {
+    fn you(&self, _opponent: &Play, value: &str) -> Play {
+        match value {
+            "X" => Play::Rock,
+            "Y" => Play::Paper,
+            "Z" => Play::Scissers,
+            _ => panic!("You '#{value}' is invalid"),
+        }
+    }
+}
+
+struct MapToOutcome {}
+
+impl Strategy for MapToOutcome {
+    fn you(&self, opponent: &Play, value: &str) -> Play {
+        match value {
+            "X" => match opponent {
+                // Loose
+                Play::Rock => Play::Scissers,
+                Play::Paper => Play::Rock,
+                Play::Scissers => Play::Paper,
+            },
+            "Y" => match opponent {
+                // Draw
+                Play::Rock => Play::Rock,
+                Play::Paper => Play::Paper,
+                Play::Scissers => Play::Scissers,
+            },
+            "Z" => match opponent {
+                // Win
+                Play::Rock => Play::Paper,
+                Play::Paper => Play::Scissers,
+                Play::Scissers => Play::Rock,
+            },
+            _ => panic!("You '#{value}' is invalid"),
+        }
+    }
 }
 
 struct Reader<'a> {
     input: &'a str,
+    strategy_type: StrategyType,
 }
 
 impl<'a> Reader<'a> {
@@ -25,67 +80,17 @@ impl<'a> Reader<'a> {
             "C" => Play::Scissers,
             _ => panic!("Opponent '#{col1}' is invalid"),
         };
-        let you = match col2 {
-            "X" => Play::Rock,
-            "Y" => Play::Paper,
-            "Z" => Play::Scissers,
-            _ => panic!("You '#{col2}' is invalid"),
-        };
+        let you = self.strategy().you(&opponent, col2);
         Game {
             opponent: opponent,
             you: you,
         }
     }
-}
 
-#[allow(dead_code)]
-pub fn total_score_2(input: &str) -> i32 {
-    games_2(input).iter().map(|game| game.score()).sum()
-}
-
-fn games_2(input: &str) -> Vec<Game> {
-    Reader2 { input: input }.games()
-}
-
-struct Reader2<'a> {
-    input: &'a str,
-}
-
-impl<'a> Reader2<'a> {
-    fn games(&self) -> Vec<Game> {
-        self.input.split("\n").map(|x| self.game(x)).collect()
-    }
-
-    fn game(&self, input: &str) -> Game {
-        let cols: Vec<&str> = input.split(" ").collect();
-        let (col1, col2) = (cols[0], cols[1]);
-        let opponent = match col1 {
-            "A" => Play::Rock,
-            "B" => Play::Paper,
-            "C" => Play::Scissers,
-            _ => panic!("Opponent '#{col1}' is invalid"),
-        };
-        let you = match col2 {
-            "X" => match opponent {
-                Play::Rock => Play::Scissers,
-                Play::Paper => Play::Rock,
-                Play::Scissers => Play::Paper,
-            },
-            "Y" => match opponent {
-                Play::Rock => Play::Rock,
-                Play::Paper => Play::Paper,
-                Play::Scissers => Play::Scissers,
-            },
-            "Z" => match opponent {
-                Play::Rock => Play::Paper,
-                Play::Paper => Play::Scissers,
-                Play::Scissers => Play::Rock,
-            },
-            _ => panic!("You '#{col2}' is invalid"),
-        };
-        Game {
-            opponent: opponent,
-            you: you,
+    fn strategy(&self) -> Box<dyn Strategy> {
+        match self.strategy_type {
+            StrategyType::MapToPlay => Box::new(MapToPlay {}),
+            StrategyType::MapToOutcome => Box::new(MapToOutcome {}),
         }
     }
 }
@@ -144,32 +149,32 @@ impl Game {
 #[cfg(test)]
 mod tests {
     use super::total_score;
-    use super::total_score_2;
+    use super::StrategyType;
     use std::fs;
 
     #[test]
     fn one_game() {
         let input = "A Y";
-        assert_eq!(total_score(&input), 8);
+        assert_eq!(total_score(&input, StrategyType::MapToPlay), 8);
     }
 
     #[test]
     fn three_game() {
         let input = "A Y\nB X\nC Z";
-        assert_eq!(total_score(&input), 15);
+        assert_eq!(total_score(&input, StrategyType::MapToPlay), 15);
     }
 
     #[test]
     fn part1() {
         let input = fs::read_to_string("src//day2/input.txt")
             .expect("Should have been able to read the file");
-        assert_eq!(total_score(&input), 9177);
+        assert_eq!(total_score(&input, StrategyType::MapToPlay), 9177);
     }
 
     #[test]
     fn part2() {
         let input = fs::read_to_string("src//day2/input.txt")
             .expect("Should have been able to read the file");
-        assert_eq!(total_score_2(&input), 12111);
+        assert_eq!(total_score(&input, StrategyType::MapToOutcome), 12111);
     }
 }
